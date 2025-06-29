3cf7e5166502a909642a602597a18e4fe6d641b8
Dhairya Kandhari <140462289+dhairyak56@users.noreply.github.com>
2025-06-29 17:19:57 +0930

    Configure CORS with fixed origin

--- server/server.js
+++ server/server.js
@@ -18,8 +18,14 @@ const { db } = require('./config/db');
 
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:3000'
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
